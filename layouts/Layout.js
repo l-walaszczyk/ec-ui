d5a3ab37b60cca55ea7c52319dfa70ea4306314f
Łukasz Walaszczyk <l.walaszczyk@interia.pl>
2020-07-24 10:44:49 +0200

    added google search console verification

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -10,6 +10,10 @@ export default function Layout({ children }) {
           src="https://kit.fontawesome.com/ecdb2bd2bc.js"
           crossOrigin="anonymous"
         ></script>
+        <meta
+          name="google-site-verification"
+          content="C4-QBjnXbQ_r5AkALSnCFORkhR3QVdpc3Hm385bRYss"
+        />
       </Head>
       <Header />
       {children}
